6c91e5b73fd694fa294d26f7cb570a91b8df5f7b
guillaume baelen <guillaume@proctorexam.com>
2021-06-19 16:46:56 +0200

    Added geocoder to get country code from latitude and longitude

--- lib/wttj_backend_test.ex
+++ lib/wttj_backend_test.ex
@@ -4,15 +4,13 @@ defmodule WttjBackendTest do
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> WttjBackendTest.hello()
-      :world
-
   """
-  def hello do
-    :world
+  def count_of_offers_per_category_per_continent do
+    %{}
+  end
+
+  def get_location_country_code(latitute, longitude) do
+    {:ok, coordinates} = Geocoder.call({latitute, longitude})
+    coordinates.location.country_code
   end
 end
--- test/wttj_backend_test_test.exs
+++ test/wttj_backend_test_test.exs
@@ -2,7 +2,15 @@ defmodule WttjBackendTestTest do
   use ExUnit.Case
   doctest WttjBackendTest
 
-  test "greets the world" do
-    assert WttjBackendTest.hello() == :world
+  test "gets fr country code" do
+    assert WttjBackendTest.get_location_country_code(48.8683316,2.3554578) == "fr"
+  end
+
+  test "gets us country code" do
+    assert WttjBackendTest.get_location_country_code(36.778259,-119.417931) == "us"
+  end
+
+  test "gets ru country code" do
+    assert WttjBackendTest.get_location_country_code(55.751244,37.618423) == "ru"
   end
 end
