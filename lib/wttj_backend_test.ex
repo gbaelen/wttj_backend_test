64fcbb815368d34249109b5b89495252075cc8ce
guillaume baelen <guillaume@proctorexam.com>
2021-06-19 15:01:36 +0200

    Initial commit

---
+++ lib/wttj_backend_test.ex
@@ -0,0 +1,18 @@
+defmodule WttjBackendTest do
+  @moduledoc """
+  Documentation for `WttjBackendTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WttjBackendTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/wttj_backend_test_test.exs
@@ -0,0 +1,8 @@
+defmodule WttjBackendTestTest do
+  use ExUnit.Case
+  doctest WttjBackendTest
+
+  test "greets the world" do
+    assert WttjBackendTest.hello() == :world
+  end
+end
