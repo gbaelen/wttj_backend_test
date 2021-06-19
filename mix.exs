ec305d53fc832d4bdbcb7e4b4130c7201389e3ce
guillaume baelen <guillaume@proctorexam.com>
2021-06-20 01:04:53 +0200

    Added continent to country mapping + csv job data extraction

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule WttjBackendTest.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:geocoder, "~> 1.1"}
+      {:geocoder, "~> 1.1"},
+      {:csv, "~> 2.4"}
     ]
   end
 end
--- test/wttj_backend_test_test.exs
+++ test/wttj_backend_test_test.exs
@@ -13,4 +13,36 @@ defmodule WttjBackendTestTest do
   test "gets ru country code" do
     assert WttjBackendTest.get_location_country_code(55.751244,37.618423) == "ru"
   end
+
+  test "gets continent for fr" do
+    assert WttjBackendTest.get_continent_from_country_code("fr") == "Europe"
+  end
+
+  test "gets continent for ru" do
+    assert WttjBackendTest.get_continent_from_country_code("ru") == "Asia"
+  end
+
+  test "gets continent for us" do
+    assert WttjBackendTest.get_continent_from_country_code("us") == "North America"
+  end
+
+  test "gets continent for ao" do
+    assert WttjBackendTest.get_continent_from_country_code("ao") == "Africa"
+  end
+
+  test "gets continent for au" do
+    assert WttjBackendTest.get_continent_from_country_code("au") == "Oceania"
+  end
+
+  test "gets continent for af" do
+    assert WttjBackendTest.get_continent_from_country_code("af") == "Asia"
+  end
+
+  test "gets category and location for correct entry" do
+    assert WttjBackendTest.extract_category_and_location(["17","FULL_TIME","Infrastructure Engineer based in Paris/Marseille","48.8867578","2.3253786"]) == ["17", "Europe"]
+  end
+
+  test "gets category and location for an entry without location" do
+    assert WttjBackendTest.extract_category_and_location(["2","FULL_TIME","Infrastructure Engineer based in Paris/Marseille","",""]) == ["2", "Unknown"]
+  end
 end
