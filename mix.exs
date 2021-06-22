6d6400cd2e7343a992a461530f591485dbda99e4
guillaume baelen <guillaume@proctorexam.com>
2021-06-22 15:38:17 +0200

    Added back script to format the csv to the project into the phoenix app as parser.ex

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule WttjBackendTest.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:geocoder, "~> 1.1"},
+      {:csv, "~> 2.4"}
     ]
   end
 
